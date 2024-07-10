9c5524131bd4b00e37cf30469fd2bc816925afbb
winternotseason <winternotseason@naver.com>
2024-07-10 17:05:08 +0900

    header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        bottom: "0 5px 5px -5px #333",
+      },
     },
   },
   plugins: [],
