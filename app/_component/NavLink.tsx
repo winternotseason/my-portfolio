f34affdbe4ca8933b8a6221f4a580b3275ba7d72
winternotseason <winternotseason@naver.com>
2024-07-15 00:32:42 +0900

    header darkmode border color

--- app/_component/NavLink.tsx
+++ app/_component/NavLink.tsx
@@ -17,8 +17,8 @@ export default function NavLink({
       href={href}
       className={
         path === href
-          ? "text-lg text-indigo-800 border-b-2 border-indigo-800 hover:text-indigo-400 hover:border-indigo-400 font-semibold"
-          : "text-lg text-indigo-800 hover:text-indigo-300"
+          ? "text-lg text-indigo-800 border-b-2 border-indigo-800 hover:text-indigo-400 hover:border-indigo-400 font-semibold dark:text-indigo-500 dark:border-indigo-500"
+          : "text-lg text-indigo-800 hover:text-indigo-300 dark:text-indigo-500"
       }
     >
       <div>{children}</div>
--- app/_component/header.tsx
+++ app/_component/header.tsx
@@ -4,7 +4,7 @@ import NavLink from "./NavLink";
 const Header = () => {
   return (
     <header className="fixed bg-white z-50 top-0 w-full flex justify-between items-center px-6 h-16 dark:bg-black">
-      <h1 className="text-indigo-800 font-bold text-2xl">Dev. SEO</h1>
+      <h1 className="text-indigo-800 font-bold text-2xl cursor-default dark:text-indigo-500">Dev. SEO</h1>
       <div className="gap-3 md:gap-10 font-medium flex text-lg">
         <NavLink href="/">About</NavLink>
         <NavLink href="/project">Project</NavLink>
