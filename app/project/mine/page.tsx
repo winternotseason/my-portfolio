5ac389e345455d629f3606d5f9b6e07c41f2df9b
winternotseason <winternotseason@naver.com>
2024-08-01 19:49:54 +0900

    mine challenges

--- app/project/mine/page.tsx
+++ app/project/mine/page.tsx
@@ -5,12 +5,12 @@ import React from "react";
 
 const Mine = () => {
   return (
-    <div className="w-full h-full flex justify-center flex-col p-4">
+    <div className="w-full h-full flex justify-center flex-col py-4 px-6">
       {/* intro */}
       <h1 className="text-base md:text-4xl font-extrabold mb-2 md:mb-4 text-center">
         <span className="text-green-500">#</span>MINE
       </h1>
-      <p className="text-base md:text-2xl font-medium md:font-semibold pb-10">
+      <p className="text-base md:text-2xl font-medium md:font-semibold pb-5">
         ► 배포 URL <br />
         <Link
           className="pl-5 mt-3 text-sm md:text-base "
@@ -20,7 +20,7 @@ const Mine = () => {
         </Link>
       </p>
       <hr />
-      <div className="w-full pt-10 space-y-10 text-sm md:text-base">
+      <div className="w-full pt-5 space-y-10 text-sm md:text-base">
         {/* Skills */}
         <div>
           <h3 className="text-base md:text-2xl font-medium md:font-semibold mb-2">
@@ -50,7 +50,7 @@ const Mine = () => {
             🖥️ Functions
           </h3>
           <p className="font-semibold">· 회원 기능</p>
-          <div className="text-xs md:text-sm pl-4 font-light md:font-normal">
+          <div className="text-xs md:text-base pl-4 font-light md:font-normal mt-1">
             <p>
               - Auth.js 라이브러리를 활용하여 안전한 회원 인증 로직을
               구현하였습니다.
@@ -65,33 +65,58 @@ const Mine = () => {
           <p className="font-semibold mt-2">
             · 카카오 맵 API를 활용한 위치 정보 기반 글 작성
           </p>
-          <p className="text-xs md:text-sm pl-4 font-light md:font-normal">
+          <p className="text-xs md:text-base pl-4 font-light md:font-normal mt-1">
             {" "}
             - 카카오 맵 API를 사용하여 사용자가 특정 음식점을 선택하고, 해당
             위치 정보를 DB에 함께 저장하여 작성된 글을 볼 때 지도와 함께
             표시되도록 구현하였습니다.
           </p>
           <p className="font-semibold mt-2">· 카테고리, 지역 필터링</p>
-          <p className="text-xs md:text-sm pl-4 font-light md:font-normal">
+          <p className="text-xs md:text-base pl-4 font-light md:font-normal mt-1">
             - Next.js의 동적 라우팅을 기반으로 params를 받아 API 호출을 통해
             필터링된 데이터들을 불러와 사용자들이 해당 카테고리와 지역에 맞는
             리뷰들을 탐색할 수 있도록 구현하였습니다.
           </p>
           <p className="font-semibold mt-2">· 상호명 검색</p>
-          <p className="text-xs md:text-sm pl-4 font-light md:font-normal">
+          <p className="text-xs md:text-base pl-4 font-light md:font-normal mt-1">
             - API Routes 내부에서 MongoDB의 $regex 함수를 사용해 상호명 검색
-            기능 구현하였습니다.
+            기능을 구현하였습니다.
           </p>
           <p className="font-semibold mt-2">· 반응형 웹사이트</p>
-          <p className="text-xs md:text-sm pl-4 font-light md:font-normal">
+          <p className="text-xs md:text-base pl-4 font-light md:font-normal mt-1">
             - Tailwind CSS를 이용하여 다양한 디바이스에서 최적화된 반응형
             웹사이트를 구현하였습니다.
           </p>
         </div>
         <div>
           <h3 className="text-base md:text-2xl font-medium md:font-semibold">
-            🚨 Trouble Shooting
+            🚨 Challenges
           </h3>
+          <p className="font-semibold mt-2">- 첫 react-query 도입의 어려움</p>
+          <p className="pl-4">
+            - 서버 상태와 클라이언트 상태를 명확히 구분하기 위해 리액트 쿼리를
+            도입하였습니다. 자주 사용되는 개념들(InfinityQuery, useMutaion,
+            useQuery...) 위주로 학습하여 적용해보았고, 나아가 사용자의 경험을 더
+            높일 수 있는 react-query의 세부적인 기능 학습의 필요성을 느꼈습니다.
+          </p>
+          <p className="font-semibold mt-2">
+            - 작성된 글 배열 데이터를 가져오기 위해 MongoDB의 find 메서드 호출
+            시 겪었던 오류
+          </p>
+          <p className="pl-4">
+            - MongoDB의 find 메서드는 배열 자체가 아닌 cursor를 return 하므로
+            toArray() 메서드를 사용하여 배열 형태로 변환하고 응답 데이터를
+            보냄으로써 문제를 해결하였습니다.
+          </p>
+          <p className="font-semibold mt-2">
+            - 글 작성 시 POST된 이미지를 cloudinary의 이미지 URL로 변환 후
+            next/image로 해당 이미지 URL을 src 속성으로 사용할 때 오류
+          </p>
+          <p className="pl-4">
+            - next/image로 외부 경로의 이미지를 불러올 때에는 hostname 설정이
+            필요하므로 next.config.mjs에서 cloudinary의 URL을 hostname으로 별도
+            설정 해줌으로써 문제를 해결하였습니다.
+          </p>
         </div>
         {/* After Working */}
         <div>
